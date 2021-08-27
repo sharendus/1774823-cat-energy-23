3220ac9121bf7aca7c085364ee732baecd855dd9
sharendus <sharendo.lyuba@mail.ru>
2021-08-27 13:24:07 +0300

    много всего

---
+++ source/js/script.js
@@ -0,0 +1,17 @@
+var menu_button = document.querySelector(".main-nav__toggle");
+var menu = document.querySelector(".main-nav");
+
+if (!menu.classList.contains("main-nav--hide") || !menu.classList.contains("main-nav--show")) {
+  menu.classList.add("main-nav--hide");
+}
+
+menu_button.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  if (menu.classList.contains("main-nav--hide")) {
+    menu.classList.remove("main-nav--hide");
+    menu.classList.add("main-nav--show");
+  } else {
+    menu.classList.remove("main-nav--show");
+    menu.classList.add("main-nav--hide");
+  }
+});
