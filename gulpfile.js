6f612285d8b21811780f21475b63236d5e1fb1bb
sharendus <sharendo.lyuba@mail.ru>
2021-09-17 13:37:30 +0300

    оптимизация правки

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,9 @@ const htmlmin = require("gulp-htmlmin");
 const csso = require("postcss-csso");
 const rename = require("gulp-rename");
 const terser = require("gulp-terser");
-const squoosh = require("gulp-libsquoosh");
+const squoosh = require("gulp-squoosh");
+const webp = require("gulp-webp");
+const del = require("del");
 
 // Styles
 
@@ -42,7 +44,7 @@ exports.html = html;
 
 // Scripts
 
-const script = () => {
+const scripts = () => {
   return gulp.src("source/js/script.js")
     .pipe(terser())
     .pipe(rename("script.min.js"))
@@ -50,19 +52,35 @@ const script = () => {
     .pipe(sync.stream());
 }
 
-exports.script = script;
+exports.scripts = scripts;
 
 // Images
 
-const images = () => {
+const optimizeImages = () => {
   return gulp.src("source/img/**/*.{png,jpg,svg}")
     .pipe(squoosh())
     .pipe(gulp.dest("build/img"))
 }
 
-exports.images = images;
+exports.optimizeImages = optimizeImages;
 
 
+const copyImages = () => {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(gulp.dest("build/img"))
+}
+
+exports.copyImages = copyImages;
+
+// WebP
+
+const createWebp = () => {
+  return gulp.src("source/img/**/*.{jpg,png}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("build/img"))
+}
+
+exports.createWebp = createWebp;
 
 // Server
 
@@ -80,15 +98,66 @@ const server = (done) => {
 
 exports.server = server;
 
+// Copy
+
+const copy = (done) => {
+  gulp.src([
+    "source/fonts/*.{woff2,woff}",
+    "source/*.ico",
+    "source/img/**/*.svg",
+  ], {
+    base: "source"
+  })
+    .pipe(gulp.dest("build"))
+  done();
+}
+
+exports.copy = copy;
+
+// Clean
+
+const clean = () => {
+  return del("build");
+};
+
 // Watcher
 
 const watcher = () => {
   gulp.watch("source/less/**/*.less", gulp.series("styles"));
   gulp.watch("source/*.html").on("change", sync.reload);
+  gulp.watch("source/js/script.js", gulp.series(scripts));
 }
 
-exports.default = gulp.series(
-  styles, html, script, images, server, watcher
+// Build
+
+const build = gulp.series(
+  clean,
+  copy,
+  optimizeImages,
+  gulp.parallel(
+    styles,
+    html,
+    scripts,
+    createWebp
+  ),
 );
 
+exports.build = build;
 
+
+// Default
+
+exports.default = gulp.series(
+  clean,
+  copy,
+  copyImages,
+  gulp.parallel(
+    styles,
+    html,
+    scripts,
+    createWebp
+  ),
+  gulp.series(
+    server,
+    watcher
+  ));
